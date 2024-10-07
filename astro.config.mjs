c04f7d42610ba364d8359808e995faefb6066f70
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-10-07 22:08:11 +0100

    Add more complicated react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import node from "@astrojs/node";
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: true,
+		}),
+	],
 	adapter: node({
 		mode: "standalone",
 	}),
